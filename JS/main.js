225a5f13fa738bf71f93bc842d0f3bbed1094185
jenifercmarques <jenifercm03@gmail.com>
2024-11-02 13:30:56 -0300

    att responsividade

--- JS/main.js
+++ JS/main.js
@@ -6,11 +6,12 @@ const setCadastro = document.querySelector('#cadastro')
 const setSend = document.querySelector('#button-send')
 const back = document.querySelector('#close-modal')
 
+
 joinNow.addEventListener('click', () => {
     cadastro.classList.add('active')
 })
 
-getNow.addEventListener('click', () =>{
+getNow.addEventListener('click', () => {
     cadastro.classList.add('active')
 })
 
@@ -22,3 +23,19 @@ setSend.addEventListener('click', () => {
 back.addEventListener("click", () => {
     send.classList.remove('active')
 })
+
+
+const animationElements = () => {
+    gsap.from(".explore", 1.5, { opacity: 0, y: 150, duration: .1 });
+    gsap.from('.class', 1.8, { opacity: 0, x: -100, delay: .1 })
+    gsap.from('.join-us', 1.5, { opacity: 0, y: 150, delay: .1 })
+    gsap.from('.plans', 1.8, { opacity: 0, x: -100, delay: .1 })
+    window.removeEventListener("scroll", animationElements)
+}
+
+window.addEventListener("scroll", animationElements)
+
+document.addEventListener("DOMContentLoaded", () => {
+    gsap.from(".home", { opacity: 0, y: 150, duration: 1.5 });
+});
+
